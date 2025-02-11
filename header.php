98a9ad7c009ad1798540b6c3cd5e24ad955143ab
Williams0000 <159028601+Williams0000@users.noreply.github.com>
2025-02-11 20:09:20 +0100

    header

--- header.php
+++ header.php
@@ -26,9 +26,21 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'underscore' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+	<div class="logo-header">
+	<?php the_custom_logo(); ?>
+	<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscore' ); ?></button>
 			<?php
-			the_custom_logo();
+			wp_nav_menu(
+				array(
+					'theme_location' => 'Menu1',
+					'menu_id'        => 'primary-menu',
+
+				)
+			);
+			?>
+</div>
+	<div class="site-branding"><?php
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -44,16 +56,18 @@
 				<p class="site-description"><?php echo $underscore_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
+			<h1 class="Titre">Laissez nous vous guider <br> vers votre maison</h1>
+			<p class="para-principal">Recherchez des propriétés sur le secteur lillois</p>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscore' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+			<div class="search-container">
+        <input type="text" class="search-input" placeholder="Keywords...">
+        <button class="search-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+        </button>
+    </div>
+		
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
